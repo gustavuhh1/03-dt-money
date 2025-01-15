0522c9314a707fe1bfc59e34c71e32bee4c4b666
GustavoMartinsR <gustavomrodrigues11@gmail.com>
2025-01-15 18:16:44 -0300

    feat: adicionar formatação de preço e data nas transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -5,6 +5,7 @@ import { NavTransactions } from "./components/NavTransactions";
 import { SearchForms } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formater";
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext);
@@ -24,11 +25,12 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === "outcome" && "- "}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               );
             })}
