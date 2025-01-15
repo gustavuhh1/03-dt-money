79b92acc3b5671698163f0ebc256a11876d4ab50
GustavoMartinsR <gustavomrodrigues11@gmail.com>
2025-01-15 16:25:03 -0300

    feat: adicionando components radix no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,23 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
-import logoImg from '../../assets/logo.svg'
+import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
-        <HeaderContent>
-            <img src={logoImg} alt="" />
+      <HeaderContent>
+        <img src={logoImg} alt="" />
 
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
-        </HeaderContent>
+          </Dialog.Trigger>
+
+          <NewTransactionModal/>
+        </Dialog.Root>
+      </HeaderContent>
     </HeaderContainer>
-  )
+  );
 }
